ee88c0ea52286e03045e36b5ce22e740a50d048e
effervescent000 <kislio01@gmail.com>
2024-02-17 09:48:34 -0500

    romance comp set up

---
+++ Source/Comp/RejectionItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Personality.Romance;
+
+public class RejectionItem : IExposable
+{
+    private Pawn pawn;
+    public int TicksSinceAsked;
+
+    public RejectionItem(Pawn pawn)
+    {
+        this.pawn = pawn;
+        TicksSinceAsked = 0;
+    }
+
+    public RejectionItem Get => this;
+
+
+    public void ExposeData()
+    {
+        Scribe_References.Look(ref pawn, "pawn");
+        Scribe_Values.Look(ref TicksSinceAsked, "ticks");
+    }
+}
---
+++ Source/Comp/RomanceComp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Personality.Romance;
+
+public class RomanceComp : ThingComp
+{
+
+    private RomanceTracker romanceTracker;
+
+
+    public RomanceTracker RomanceTracker => romanceTracker;
+
+    public override void PostExposeData()
+    {
+        Scribe_Deep.Look(ref romanceTracker, "romance");
+    }
+
+}
