f70ff521d6943837973af4749ab18b5113533a8d
effervescent000 <kislio01@gmail.com>
2024-03-17 09:14:18 -0400

    cleanup

--- Source/GUI/MindCardUtility.cs
+++ Source/GUI/MindCardUtility.cs
@@ -62,48 +62,6 @@ public static class MindCardUtility
         return yStart;
     }
 
-    //private static void DrawLikesAndDislikes(Pawn pawn, List<string> likes, List<string> dislikes, Rect rect)
-    //{
-    //    float yStart = rect.y;
-    //    if (likes.Count > 0)
-    //    {
-    //        Text.Font = GameFont.Small;
-    //        Rect labelRect = new(rect.x, rect.y, LabelWidth, Text.CalcHeight(LikesText, LabelWidth));
-    //        Widgets.Label(labelRect, LikesText);
-    //        foreach (var like in likes)
-    //        {
-    //            Text.Font = GameFont.Tiny;
-    //            Vector2 cardSize = Text.CalcSize(like);
-    //            Rect cardRect = new(rect.x + LabelWidth + 5f, yStart, cardSize.x, cardSize.y);
-    //            Widgets.Label(cardRect, like);
-    //            yStart += cardSize.y;
-    //        }
-    //        if (yStart < labelRect.height - rect.y)
-    //        {
-    //            yStart = labelRect.yMax;
-    //        }
-    //    }
-
-    //    if (dislikes.Count > 0)
-    //    {
-    //        Text.Font = GameFont.Small;
-    //        Rect labelRect = new(rect.x, yStart, LabelWidth, Text.CalcHeight(DislikesText, LabelWidth));
-    //        Widgets.Label(labelRect, DislikesText);
-    //        foreach (var dislike in dislikes)
-    //        {
-    //            Text.Font = GameFont.Tiny;
-    //            Vector2 cardSize = Text.CalcSize(dislike);
-    //            Rect cardRect = new(rect.x + LabelWidth + 5f, yStart, cardSize.x, cardSize.y);
-    //            Widgets.Label(cardRect, dislike);
-    //            yStart += cardSize.y;
-    //        }
-    //        if (yStart < labelRect.height - rect.y)
-    //        {
-    //            yStart = labelRect.yMax;
-    //        }
-    //    }
-    //}
-
     private static float CalcSizeOfSection(List<Preference> itemsToRender)
     {
         if (itemsToRender.Count == 0) return 0f;
